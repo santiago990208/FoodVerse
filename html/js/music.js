71cca3f89f56f4a7fd1576f0a5ae0d1a0b09a5b7
santiago990208 <santibotero2012@gmail.com>
2022-10-11 11:13:30 -0500

    Create restaurant model index

---
+++ html/js/music.js
@@ -0,0 +1,7 @@
+ //bg music handler
+ document.querySelector("button").onclick = function() {
+     document.querySelector("#loadingScreen").style.display = "none"
+     document.querySelector("#bgSoundtrack").volume = 0.09;
+     document.querySelector("#bgSoundtrack").loop = true;
+     document.querySelector("#bgSoundtrack").play()
+   };

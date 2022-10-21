3bd1f081b2aae219c57917ba61da17ac752e0d3b
josephrojas <jorogo2001@hotmail.com>
2022-10-21 02:03:50 -0500

    ading dashboard

---
+++ admin/src/App.js
@@ -0,0 +1,17 @@
+import "./app.css";
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./pages/home/Home";
+function App() {
+  return (
+    <div>
+      <Topbar/>
+      <div className="container">
+        <Sidebar/>
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ admin/src/components/topbar/Topbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./topbar.css";
+import { LanguageOutlined, Settings, NotificationsActive } from '@material-ui/icons';
+export default function Topbar() {
+    return (
+        <div className='topbar'>
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">admin</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsActive />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <LanguageOutlined />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+                    </div>
+                    <img src="https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock--480x320.jpg" alt="" className="topAvatar" />
+                </div>
+            </div>
+        </div>
+    )
+}

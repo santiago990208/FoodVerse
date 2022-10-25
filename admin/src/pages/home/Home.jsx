5dcfac97151fe91e0dac7a2f198de9c86ee9b315
josephrojas <jorogo2001@hotmail.com>
2022-10-24 21:32:43 -0500

    9:32 pm
    adding maps

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -3,14 +3,16 @@ import BarChart from "../../components/barchart/BarChart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
 import { userData, saleData } from "../../dummyData";
+import Map from "../../components/Map/Map";
 
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
       <Chart data={userData} title="Ventas por mes" grid dataKey="Promedio"/>
+
       <BarChart data={saleData} title="Productos más vendidos" grid dataKey="Ventas" xBar = "producto"/>
-      
+      <Map title = "HeatMap"></Map>
     </div>
   );
 }
